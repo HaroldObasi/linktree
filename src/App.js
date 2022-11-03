c49e0afff7e9d3d7e23bc32a468ec04c4e136015
HaroldObasi <haroldobasi2k16@gmail.com>
2022-11-03 16:21:57 +0100

    added footer to root

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Home from "./screens/Home";
+import Footer from "./components/Footer";
 import Contact from "./screens/Contact";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
     return (
-        <Router>
-            <Routes>
-                <Route exact path="/" element={<Home />} />
-                <Route path="/contact" element={<Contact />} />
-            </Routes>
-        </Router>
+        <>
+            <Router>
+                <Routes>
+                    <Route exact path="/" element={<Home />} />
+                    <Route path="/contact" element={<Contact />} />
+                </Routes>
+            </Router>
+            <Footer />
+        </>
     );
 }
 
